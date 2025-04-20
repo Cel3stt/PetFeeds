d6c48a595020ea7b2ca60eb3f26911c01be29ba7
ayrelmay <152473022+ayrelmay@users.noreply.github.com>
2025-04-21 02:54:34 +0800

    snapshot backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import { connectDB } from './database/connectDB.js';
-import scheduleRoutes  from './routes/schedule.routes.js'
-
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { connectDB } from "./database/connectDB.js";
+import scheduleRoutes from "./routes/schedule.routes.js";
+import snapshotRoutes from "./routes/snapshot.route.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -13,11 +14,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+//upload path
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
 // Routes
-app.use('/api/schedule', scheduleRoutes);
+app.use("/api/schedule", scheduleRoutes);
+app.use("/api/snapshot", snapshotRoutes);
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log("Server is running on port", PORT);
-  });
-  
+  connectDB();
+  console.log("Server is running on port", PORT);
+});
