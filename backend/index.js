7defc2df2592ca9ed7d8b7cd6e2f091fcd13dc03
kiotsukettene <kiotsuketteneloreto@gmail.com>
2025-04-22 06:34:17 +0800

    changed

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import path from "path";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-const ESP32_IP = '192.168.0.100'; // Static IP
+const ESP32_IP = '192.168.29.118'; // Static IP
 
 app.use(cors());
 app.use(express.json());
