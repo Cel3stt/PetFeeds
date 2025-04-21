30ebd7e42cadee11379469d0188a930702cf4e23
kiotsukettene <kiotsuketteneloreto@gmail.com>
2025-04-22 03:02:09 +0800

    back to 3000 frontend

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 const isDev = import.meta.env.MODE === 'development';
 
-export const API_URL = isDev ? 'http://localhost:4000' : 'http://34.232.21.31:5000';
+export const API_URL = isDev ? 'http://localhost:4000' : 'http://34.232.21.31:3000';
 export const ESP32_IP = import.meta.env.VITE_ESP32_IP || '192.168.0.100';
 export const CAMERA_IP = import.meta.env.VITE_CAMERA_IP || '192.168.29.77';
