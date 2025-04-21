d470f3b64416c363ae3fdff2fedef3da523f003b
kiotsukettene <kiotsuketteneloreto@gmail.com>
2025-04-22 05:01:46 +0800

    add for production

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import path from "path";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
-const ESP32_IP = '192.168.0.100'; // Static IP
+const ESP32_IP = '192.168.29.118'
 
 app.use(cors());
 app.use(express.json());
